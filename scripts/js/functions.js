a8ba68d4a2eee5f17857aa087681de43929a53ca
DrMikeyS <mike@mikedavidsmith.com>
2021-01-13 14:16:52 +0000

    Add files via upload

--- scripts/js/functions.js
+++ scripts/js/functions.js
@@ -118,3 +118,147 @@ csvResult.forEach(function(patient, index) {
         });
     });
 }
+
+function genFormHTML(csvResult,keys){
+    var fullhtml = '';
+    csvResult.forEach(function(patient, index) {
+        html = genFullPageHTML(patient, index,keys);
+        fullhtml = fullhtml + html;
+    });
+    return fullhtml;
+}
+
+
+function genFullPageHTML(patient, index,keys){
+    return `<div class="vaccine-form"><h1>Vaccine Record Form</h1>
+<table class="table table-bordered">
+    <tr>
+        <td>Name</td>
+        <td>` + patient.Name + `</td>
+        <td>Address</td>
+        <td>` + patient[keys['address']] + `</td>
+    </tr>
+    <tr>
+        <td>NHS No.</td>
+        <td>` + patient[keys['nhsno']] + `</td>
+        <td>DOB</td>
+        <td>` + formatDate(patient[keys['dob']]) + `</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><div class="qr-code" id="nhs-qr-` + index + `"></div></td>
+        <td></td>
+        <td><div class="qr-code" id="dob-qr-` + index + `"></div></td>
+    </tr>
+    <tr>
+        <td>Sex</td>
+        <td></td>
+        <td>GP Practice</td>
+        <td></td>
+    </tr>
+</table>
+
+<table class="table table-bordered">
+    <tr>
+        <th>Screening Questions</th>
+        <th>Yes</th>
+        <th>No</th>
+    </tr>
+    <tr>
+        <td>1. Are you currently unwell with fever?</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>2. Have you ever had any serious allergic reaction?</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>3. Have you ever been prescribed an adrenaline autoinjector such as EpiPen?</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>4. Are you or have you been in a trial of a potential coronavirus vaccine?</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>5. Are you, or could you be pregnant, breastfeeding or planning to become pregnant in the next three months?</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>6. Are you taking anticoagulant medication, or do you have a bleeding disorder?</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>7. Have you had any vaccinations in the last seven days?</td>
+        <td></td>
+        <td></td>
+    </tr>
+
+    <table class="table table-bordered">
+        <tr>
+            <th colspan="2">Consent</th>
+        </tr>
+        <tr>
+            <td>Consent given?</td>
+            <td>Yes | No</td>
+        </tr>
+        <tr>
+            <td>Consent Provided By:</td>
+            <td>Patient | LPA for Health | Court Appointed Deputy | Clinician Best Interests Decision (MCA)</td>
+        </tr>
+        <tr>
+            <td>Notes</td>
+            <td></td>
+        </tr>
+    </table>
+
+
+    <table class="table table-bordered">
+        <tr>
+            <th colspan="2">Vaccination Details</th>
+        </tr>
+        <tr>
+            <td>Date of Vaccination</td>
+            <td>` + patient.SessionDate + `</td>
+
+        </tr>
+        <tr>
+            <td>Time of Vaccination (24hr)</td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>Batch Number
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Administration Site
+            </td>
+            <td>               Left    |    Right   |      Deltoid   |      Thigh</td>
+        </tr>
+        <tr>
+            <td>Any Adverse Effects (blank for none) or other comments
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Vaccinator Name
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Vaccine not given (reason)
+            </td>
+            <td>Unwell | Contraindicated | Did not consent</td>
+        </tr>
+    </table>
+    </div>
+    <div class="page-break-clear"></div><div class="page-break">&nbsp;</div>`;
+}
